1728bd27b188665ce65b7f4888e325f5b4480f78
Vitaly Tihonov <v.tikhonov@gt-sot.ru>
2021-08-14 14:02:34 +0300

    Made pagination. Styling the more button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.scss';
 import Flight from './components/Flight/Flight';
 import FilterPanel from './components/FilterPanel/FilterPanel';
@@ -8,19 +8,73 @@ import { result } from './data/flights.json';
 const { flights } = result;
 
 function App() {
+  const [sortedIndices, setSortedIndices] = useState(flights.map((_, i) => i));
+  const incrementSize = 2;
+  const [renderedIndices, setRenderedIndices] = useState(sortedIndices.slice(0, incrementSize));
+
+  function renderMore() {
+    if (sortedIndices.length - renderedIndices.length >= incrementSize) {
+      setRenderedIndices(sortedIndices.slice(0, renderedIndices.length + incrementSize))
+    } else {
+      setRenderedIndices(sortedIndices)
+    }
+  }
+
   return (
     <div className="app">
-      <div className="app__left-column" >
+      <div className="filter" >
         <FilterPanel />
       </div>
 
       <section className="results" >
-        {flights.map((flight, index) => <Flight data={flight} key={index} />)}
+        <div className="results__view-area" >
+          {renderedIndices.map((index, i) => <Flight data={flights[index]} key={i} />)}
+        </div>
 
-        <button className="results__button" >Показать еще</button>
+        <div className="results__button-background" >
+          <button className="results__button" onClick={renderMore} >Показать еще</button>
+        </div>
       </section>
     </div>
   );
 }
 
 export default App;
+
+/*
+VARIANT A
+
+sortedIndices
+incrementSize
+renderedIndices useState(sortedIndices.slice(0, incrementSize));
+
+renderMore()
+  // sortedIndices.length
+  // renderedIndices.length
+  // incrementSize
+  if (sortedIndices.length - renderedIndices.length >= incrementSize) // добавить к renderedIndices incrementSize
+    setRenderedIndices(sortedIndices.slice(0, renderedIndices.length + incrementSize))
+  else // отрендерить все sortedIndices
+    setRenderedIndices(sortedIndices)
+
+return
+  {renderedIndices.map((index, i) => <Flight data={flights[index]} key={i} />)}
+*/
+
+/*
+VARIANT B
+
+sortedIndices
+incrementSize
+lastRenderedIndex = useState ( incrementSize - 1 )
+
+renderMore
+  const newIndex = lastRenderedIndex + incrementSize;
+  setLastRenderedIndex(newIndex < sortedIndices.length ? newIndex : sortedIndices.length + 1);
+
+return
+  {sortedIndices.map((index, i) => index <= lastRenderedIndex && <Flight data={flights[index]} key={i} />)}
+
+  так map продолжит итерироваться по sortedIndices, сравнивая индексы, до самого конца. Вероятно, вариант А оптимальнее,
+  т. к. в нем рендер не имеет этого недостатка, а renderMore хоть и кажется сложнее, но эта операция выполняется реже, чем рендер
+*/
